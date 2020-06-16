b756347d84730b88bc44b6f71964c8c489d3fb27
gido5731 <celtictarot386@gmail.com>
2020-06-16 01:11:09 -0400

    This is as basic of an implementation at the backend level as it gets, but now we can call it "lightweight"!

--- ProjectTapebucket.go
+++ ProjectTapebucket.go
@@ -8,11 +8,15 @@ import (
 	"github.com/gidoBOSSftw5731/log"
 )
 
+const (
+	URLLen = 8
+)
+
 // AddToDB adds a block of text to the database and returns both the url and error
 func AddToDB(text, identity *string, db *sql.DB) (string, error) {
 	textHash := fmt.Sprintf("%x", sha256.Sum256([]byte(*text)))
 
-	url := textHash[:8]
+	url := textHash[:URLLen]
 
 	test := db.QueryRow("SELECT hash FROM tapebucket WHERE url=?", url)
 	switch {
@@ -27,6 +31,23 @@ func AddToDB(text, identity *string, db *sql.DB) (string, error) {
 		//defer insert.Close()
 		log.Debug("Added paste to table")
 	case test.Scan().Error() != "":
-		return "", fmt.Errorf(test.Scan().Error())		
+		return "", fmt.Errorf(test.Scan().Error())
 	}
+
+	return url, nil
+}
+
+// ReturnFromDB returns a pointer to the paste from a database and an error.
+func ReturnFromDB(url string, db *sql.DB) (*string, error) {
+	if len(url) != URLLen {
+		return nil, fmt.Errorf("badurlformat")
+	}
+	// so now we know the URL could be valid
+
+	var paste string
+	err := db.QueryRow("SELECT hash FROM tapebucket WHERE url=?", url).Scan(&paste)
+	if err != nil {
+		return nil, err
+	}
+	return &paste, nil
 }
